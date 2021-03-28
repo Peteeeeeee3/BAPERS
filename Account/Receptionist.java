f131134c6eba2c2306687aee8e2657f6d0c08850
Peter Farkas <peter.farkas@city.ac.uk>
2021-03-28 06:59:12 +0100

    a

--- Account/Receptionist.java
+++ Account/Receptionist.java
@@ -168,8 +168,8 @@ public class Receptionist extends UserAccount {
         }
     }
 
-    public Receptionist(UserAccount user) {
-        super(user.getStaffID(), user.getPassword(), user.getName(), user.getAccess());
-        throw new UnsupportedOperationException();
-    }
+    //public Receptionist(UserAccount user) {
+      //  super(user.getStaffID(), user.getPassword(), user.getName(), user.getAccess());
+      //  throw new UnsupportedOperationException();
+   // }
 }
